72b188e8154ee57dc16f213de439786f2e2bfa2c
Clément COQUILLE <cc.lle@protonmail.com>
2021-10-25 22:14:37 +0200

    fix email present twice in Administrator

--- db/migrate/20211025200957_devise_create_administrators.rb
+++ db/migrate/20211025200957_devise_create_administrators.rb
@@ -14,13 +14,6 @@ class DeviseCreateAdministrators < ActiveRecord::Migration[6.1]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
-
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
@@ -31,8 +24,6 @@ class DeviseCreateAdministrators < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-      t.string :email
 
       t.timestamps null: false
     end
