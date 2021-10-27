ca1ed3f2629f11c24bb04c060a141ff6ed54e407
Clément COQUILLE <cc.lle@protonmail.com>
2021-10-27 13:02:15 +0200

    add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,11 @@ require 'faker'
   Document, Comment, Event, Contact, Administrator, User
 ].map(&:destroy_all)
 
+Administrator.create!(
+  email:    'foo@bar.com',
+  password: 'foobar'
+)
+
 20.times do
   user = User.create!(
     fullname:    Faker::Name.name,
@@ -12,8 +17,13 @@ require 'faker'
 
   Contact.create!(
     user:   user,
-    mobile: Faker::PhoneNUmber.cell_phone_in_e164,
+    mobile: Faker::PhoneNumber.cell_phone_in_e164,
     email:  Faker::Internet.email
+  )
+
+  Administrator.create!(
+    email:    Faker::Internet.email,
+    password: 'foobar'
   )
 end
 
@@ -31,16 +41,38 @@ end
 
 70.times do
   document = Document.create!(
-    title: Faker::Lorem.paragraph(sentence_count: 1),
-    document_type: 'Event',
-    document_id: Event.all.sample
+    title:             Faker::Lorem.paragraph(sentence_count: 1),
+    documentable_type: 'Event',
+    documentable_id:   Event.all.sample.id
   )
 
-  document.attach(io: File.open(Rails.root.join('storage', 'seed.txt')))
+  document.file.attach(io:           File.open(Rails.root.join('storage', 'seed.txt')),
+                       filename:     'seed.txt',
+                       content_type: 'text/plain',
+                       identify:     false)
 end
 
 100.times do
   Comment.create!(
-    body: Faker::Lorem.paragraph(sentence_count: 1..20),
+    administrator:    Administrator.all.sample,
+    body:             Faker::Lorem.paragraph(sentence_count: 1..20),
+    commentable_type: 'Event',
+    commentable_id:   Event.all.sample.id,
+  )
+
+  Comment.create!(
+    administrator:    Administrator.all.sample,
+    body:             Faker::Lorem.paragraph(sentence_count: 1..20),
+    commentable_type: 'Document',
+    commentable_id:   Document.all.sample.id,
+  )
+end
+
+20.times do
+  Comment.create!(
+    administrator:    Administrator.all.sample,
+    body:             Faker::Lorem.paragraph(sentence_count: 1..20),
+    commentable_type: 'User',
+    commentable_id:   User.all.sample.id,
   )
 end
