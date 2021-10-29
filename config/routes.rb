6a771bca6afe5b8fcab23865d468e7c97929b19f
Clément COQUILLE <cc.lle@protonmail.com>
2021-10-29 10:58:58 +0200

    add Bootstrap 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :administrators
-  resources :documents
-  resources :events
-  resources :contacts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'calendars#index'
+
+  resources :comments, :documents, :events, :contacts, :users
 end
