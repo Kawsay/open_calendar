7d0055eb7f3231b1fdd94213a060d14fec744025
Clément COQUILLE <cc.lle@protonmail.com>
2021-10-29 17:02:09 +0200

    add AJAX calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,10 @@
 class CalendarsController < ApplicationController
   def index
     @events = Event.all.includes(:user)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
