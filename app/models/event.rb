d322f3475c3855ad540d46c6246f28ee6100580f
Clément COQUILLE <cc.lle@protonmail.com>
2021-11-02 16:16:20 +0100

    add truncated text to calendar events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :documents, as: :documentable
   has_many :comments, as: :commentable
+
+  def truncated_description(word_count: 8)
+    description.split(/\s+/, word_count + 1)[0...word_count].append('...').join(' ')
+  end
 end
