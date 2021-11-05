3b6b984800e481fb60c38882b079fada8ae82e46
Clément COQUILLE <cc.lle@protonmail.com>
2021-11-05 15:13:34 +0100

    add User#index & #show views

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,18 @@ class Event < ApplicationRecord
   def truncated_description(word_count: 8)
     description.split(/\s+/, word_count + 1)[0...word_count].append('...').join(' ')
   end
+
+  def date
+    if end_date.nil?
+      "Le #{format_date(start_date)}"
+    else
+      "Du #{format_date(start_date)} au #{format_date(end_date)}"
+    end
+  end
+
+  private
+
+  def format_date(date)
+    date.strftime('%d/%m/%Y %H:%M')
+  end
 end
