de56c7d06c0fb9094940475fda21ce0f3b8a0dbc
kawsay <cc.lle@protonmail.com>
2021-11-25 15:10:28 +0100

    add authorization 401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_administrator!
+
+  def page_unauthorized
+    if administrator_signed_in?
+      redirect_to login_redirect_path
+    else
+      render file: 'public/401.html', status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_redirect_path
+    stored_location_for(:administrator) || root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :calendars, :contacts, :comments, :documents, :events, :users
 
   get 'events_modal/:id', to: 'events#_show_modal', as: 'events_modal'
+
+  # Errors
+  get '/401', to: 'application#page_unauthorized', as: :page_unauthorized
 end
