f1ea32b9fc911277a4d8426e7ce14984e3dd0902
kawsay <cc.lle@protonmail.com>
2022-02-01 21:23:11 +0100

    config css loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
+const CSSLoader = environment.loaders
+  .get('moduleSass')
+  .use
+  .find((el) => el.loader === 'css-loader')
+
 module.exports = environment
