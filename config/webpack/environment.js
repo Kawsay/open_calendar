b53941accce31056e78783cc2de183fdbc2d2f52
kawsay <cc.lle@protonmail.com>
2022-02-01 21:29:04 +0100

    config css loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
 environment.loaders.insert('sass', {
-    test: /\.scss$/,
-    use: [
-        "to-string-loader", // creates style nodes from JS strings
-        "css-loader", // translates CSS into CommonJS
-        "sass-loader" // compiles Sass to CSS
-    ]
+  test: /\.scss$/,
+  use: [
+    "css-loader", // translates CSS into CommonJS
+    "sass-loader" // compiles Sass to CSS
+  ]
 });
 
 module.exports = environment
