9c0134d88d5741bdefb5663b5f5faa92ad70b076
kawsay <cc.lle@protonmail.com>
2022-02-02 15:21:45 +0100

    fix N+1 on EventType

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,6 @@
 class CalendarsController < ApplicationController
   def index
-    @events = Event.all.includes(:user)
+    @events = Event.all.includes(:user, :event_type)
 
     respond_to do |format|
       format.html
