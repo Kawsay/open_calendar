08853725bf3a17bfa39d5b5a98db899b76dedaaa
kawsay <cc.lle@protonmail.com>
2022-02-04 17:21:45 +0100

    remove webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
-environment.loaders.insert('sass', {
-  test: /\.scss$/,
-  use: [
-    "css-loader", // translates CSS into CommonJS
-    "sass-loader" // compiles Sass to CSS
-  ]
-});
+// environment.loaders.insert('sass', {
+//   test: /\.scss$/,
+//   use: [
+//     "css-loader", // translates CSS into CommonJS
+//     "sass-loader" // compiles Sass to CSS
+//   ]
+// });
 
 module.exports = environment
