17d583f12ccaa1ce070f25023425a3f23f443bb4
kawsay <cc.lle@protonmail.com>
2022-02-05 20:15:29 +0100

    add icons

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,8 +1,9 @@
 class CalendarsController < ApplicationController
   def index
-    @events = Event.all.includes(:user, :event_type)
-    @event  = Event.new
-    @users  = User.select(:id, :fullname)
+    @calendars = Calendar.all.includes(:events)
+    @events    = Event.all
+    @event     = Event.new
+    @users     = User.select(:id, :fullname)
 
     respond_to do |format|
       format.html
