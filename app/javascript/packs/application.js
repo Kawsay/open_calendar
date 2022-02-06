21cfe7646ac03e1f8fa09d0ef21670645d038ddb
kawsay <cc.lle@protonmail.com>
2022-02-06 16:10:18 +0100

    test specify image extention

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,10 @@ require("@rails/actiontext")
 import './customs/enable_tooltips'
 
 import EventModalFormModifier from './components/EventModalFormModifier'
+import CalendarDisplayToggler from './components/CalendarDisplayToggler'
 
 const components = {
-  'calendars/index': [EventModalFormModifier]
+  'calendars/index': [EventModalFormModifier, CalendarDisplayToggler]
 };
 
 
