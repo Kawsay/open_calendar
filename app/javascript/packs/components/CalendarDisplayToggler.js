d5f56f6429769ac003fe7ca8103c0b6badf9dfad
kawsay <cc.lle@protonmail.com>
2022-02-07 13:57:29 +0100

    wip fix toggle eventBadge

--- app/javascript/packs/components/CalendarDisplayToggler.js
+++ app/javascript/packs/components/CalendarDisplayToggler.js
@@ -23,33 +23,30 @@ var _bindEvents = function() {
       var calendarState = calendarEyeIcon.getAttribute('data-calendar-state');
       var calendarName  = calendarEyeIcon.getAttribute('data-calendar-name');
 
-      console.log(calendarState)
-      console.log(calendarName)
-
-      _toggleEventBadges(calendarState, calendarName);
+      _toggleEventBadges(calendarState, calendarName, calendarEyeIcon);
     });
   };
 };
 
-var _toggleEventBadges = function(calendarState, calendarName) {
+var _toggleEventBadges = function(calendarState, calendarName, calendarEyeIcon) {
   var filteredEventBadges = calendarEventBadgesArr.filter((eventBadge) => {
-    _belongsToThisCalendar(eventBadge);
+    return eventBadge.getAttribute('data-calendar-name') === calendarName
   });
 
   filteredEventBadges.map((eventBadge) => {
-    _toggleEventBadge(eventBadge, calendarState);
+    _toggleEventBadge(eventBadge, calendarState, calendarEyeIcon);
   });
 };
 
-var _belongsToThisCalendar = function(eventBadge, calendarName) {
-  return eventBadge.getAttribute('data-calendar-name') === calendarName;
-};
-
-var _toggleEventBadge = function(eventBadge, calendarState) {
+var _toggleEventBadge = function(eventBadge, calendarState, calendarEyeIcon) {
   if (calendarState === 'displayed') {
     eventBadge.style.display = 'none';
+    eventBadge.classList.remove('d-grid');
+    calendarEyeIcon.setAttribute('data-calendar-state', 'hidden');
   } else if (calendarState === 'hidden') {
     eventBadge.style.display = 'inline-block !important'
+    eventBadge.classList.add('d-grid');
+    calendarEyeIcon.setAttribute('data-calendar-state', 'displayed');
   };
 };
 
