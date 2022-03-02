60304e98233a0b19564dbb4444439125edea18b4
kawsay <cc.lle@protonmail.com>
2022-03-02 13:15:57 +0100

    remove :team_member_id from Team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_175028) do
+ActiveRecord::Schema.define(version: 2022_03_02_121320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -152,10 +152,8 @@ ActiveRecord::Schema.define(version: 2022_02_28_175028) do
   create_table "teams", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "team_member_id"
     t.string "name", null: false
     t.integer "visit_count"
-    t.index ["team_member_id"], name: "index_teams_on_team_member_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
