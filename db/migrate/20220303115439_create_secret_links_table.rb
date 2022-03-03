ba18281d75b4cf646754c22312b09f6355d71e72
kawsay <cc.lle@protonmail.com>
2022-03-03 13:33:31 +0100

    fix migration table name

--- db/migrate/20220303115439_create_secret_links_table.rb
+++ db/migrate/20220303115439_create_secret_links_table.rb
@@ -1,6 +1,6 @@
 class CreateSecretLinksTable < ActiveRecord::Migration[6.1]
   def change
-    create_table :secret_links_tables do |t|
+    create_table :secret_links do |t|
       t.string :slug, limit: 16, null: false, default: SecureRandom.alphanumeric
       t.integer :validity_period, null: false, default: 1
       t.integer :visit_count, null: false, default: 0
