0a7fd37e0738c63d2c3e44c6dcc3d1b2dee3280c
kawsay <cc.lle@protonmail.com>
2022-03-08 19:48:48 +0100

    add api/v1/events#index + event scopes

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,14 +1,9 @@
 class Api::V1::EventsController < Api::BaseController
   def index
-    @events = Event.joins(calendar: :team)
-                   .where('start_date >= ? AND start_date < ?',
-                          event_params[:start_date], event_params[:end_date])
-                   .where('"teams"."name" = ?',
-                          event_params[:team_name])
-
-    respond_to do |format|
-      format.json
-    end
+    @events = Event.of_team(event_params[:team_name])
+                   .between_dates(event_params[:start_date], event_params[:end_date])
+  rescue ActionController::ParameterMissing
+      render json: { errors: ['Invalid request: missing parameters'] }, status: :bad_request
   end
 
   private
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,12 +7,17 @@ class Event < ApplicationRecord
 
   has_rich_text :description
 
-  scope :future, -> { where('start_date > ?', DateTime.now) }
-  scope :past, -> { where('start_date < ?', DateTime.now) }
   scope :of_calendars, ->(calendars) { where(calendar: calendars) }
+  scope :of_team, ->(team_name) { joins(calendar: :team).where(team: { name: team_name }) }
+  scope :future, -> { where('start_date >= ?', DateTime.now) }
+  scope :past, -> { where('start_date < ?', DateTime.now) }
+  scope :after, ->(date) { where('start_date >= ?', date) }
+  scope :before, ->(date) { where('start_date < ?', date) }
+  scope :between_dates, ->(start_date, end_date) { after(start_date).before(end_date) }
 
   validates_presence_of :title
   validates_presence_of :calendar_id
+  validates_presence_of :start_date
 
   def truncated_description(word_count: 5)
     description.split(/\s+/, word_count + 1)[0...word_count].append('...').join(' ')
@@ -26,7 +31,7 @@ class Event < ApplicationRecord
       "Du #{format_date(start_date)} au #{format_date(end_date)}"
     end
   end
-  
+
   # Set :end_date from Flatpickr String date if a range date is passed
   def check_for_range_date!
     binding.pry
