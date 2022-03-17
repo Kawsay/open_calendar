c3e990fd4cd73c1b5eedb819f20b37890443b569
kawsay <cc.lle@protonmail.com>
2022-03-17 19:46:34 +0100

    remove unused code in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,27 +18,8 @@ class Event < ApplicationRecord
   validates_presence_of :calendar_id
   validates_presence_of :start_date
 
-  def truncated_description(word_count: 5)
-    description.split(/\s+/, word_count + 1)[0...word_count].append('...').join(' ')
-  end
-
-  # TODO: I18n
-  def date
-    if end_date.nil?
-      "Le #{format_date(start_date)}"
-    else
-      "Du #{format_date(start_date)} au #{format_date(end_date)}"
-    end
-  end
-
   # Set :end_date from Flatpickr String date if a range date is passed
   def check_for_range_date!
     binding.pry
   end
-
-  private
-
-    def format_date(date)
-      date.strftime('%d/%m/%Y %H:%M')
-    end
 end
