2b322f6585147a8fae41c7d1f7b4bf27291201bb
kawsay <cc.lle@protonmail.com>
2022-03-27 14:26:20 +0200

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,32 +3,41 @@
 Rails.application.routes.draw do
   root to: 'teams#show'
 
+  #
+  # Authentication
+  #
+  devise_for :users
+
   use_doorkeeper do
     skip_controllers :authorizations, :applications, :authorized_applications
   end
 
+  #
+  # API
+  #
   namespace :api do
     namespace :v1 do
       resources :events, only: %i[index]
     end
   end
 
-  devise_for :users
-
-  get '/calendars/:slug', to: 'secret_links#show', constraint: { slug: /\A[a-zA-Z0-9]{16}\z/ }
-
+  #
+  # Application
+  #
   resources :teams do
     resources :calendars do
       resources :events, shallow: true
     end
   end
-  get '/events/:id/update_modal', to: 'events#update_modal'
 
-  resources :organizations
+  resources :organizations, only: %i[create]
   resources :secret_links, only: %i[create]
 
-  post 'reset_teams', to: 'teams#destroy_all', as: 'reset_team'
+  get '/events/:id/update_modal', to: 'events#update_modal'
+  post 'reset_teams',             to: 'teams#destroy_all', as: 'reset_team' # TODO: remove before v0
 
+  #
   # Errors
+  #
   get '/401', to: 'application#page_unauthorized', as: :page_unauthorized
 end
