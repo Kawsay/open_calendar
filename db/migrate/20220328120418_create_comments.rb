857894f90d66a5a4155378503b2860cc552c4f4a
kawsay <cc.lle@protonmail.com>
2022-03-29 19:52:28 +0200

    create comments table

--- db/migrate/20220328120418_create_comments.rb
+++ db/migrate/20220328120418_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments, id: :uuid do |t|
       t.belongs_to :user, null: false, foreign_key: true, type: :uuid
-      t.belongs_to :commentable, polymorphic: true, null: false
+      t.belongs_to :commentable, polymorphic: true, null: false, type: :uuid
       t.integer :parent_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_120418) do
   create_table "comments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "user_id", null: false
     t.string "commentable_type", null: false
-    t.bigint "commentable_id", null: false
+    t.uuid "commentable_id", null: false
     t.integer "parent_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
