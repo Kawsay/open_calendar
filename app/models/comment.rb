9e3ea5871ce176d159f7af51f54e42e1a523a05f
kawsay <cc.lle@protonmail.com>
2022-03-31 18:57:57 +0200

    WIP fix N+1 on nested comments: Recursive SQL

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -24,4 +24,24 @@ class Comment < ApplicationRecord
     broadcast_remove_to self
     broadcast_action_to self, action: :remove, target: "#{dom_id(self)}_with_comments"
   end
+
+  # def self.recursive_find(id)
+  #   sql = <<-SQL.squish
+  #     WITH RECURSIVE comment_tree_cte
+  #     AS (
+  #       SELECT id, parent_id, commentable_id, user_id, created_at
+  #       FROM comments
+  #       WHERE commentable_id = :id
+  #       UNION
+  #         SELECT comments.id, comments.parent_id, comments.commentable_id, comments.user_id, comments.created_at
+  #         FROM comments
+  #       JOIN comment_tree_cte ON comments.parent_id = comment_tree_cte.id
+  #     )
+  #     SELECT * FROM comment_tree_cte;
+  #   SQL
+
+  #   records = find_by_sql([sql, id: id])
+  #   ActiveRecord::Associations::Preloader.new.preload(records, [:rich_text_body, :user])
+  #   records
+  # end
 end
