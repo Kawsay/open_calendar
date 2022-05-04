9bc8c2bbfa1d098480b801c35282774ffc8dc7d3
kawsay <cc.lle@protonmail.com>
2022-05-04 14:52:44 +0200

    [wip] hide popovers on outside clicks

--- app/javascript/packs/controllers/popover_controller.js
+++ app/javascript/packs/controllers/popover_controller.js
@@ -4,16 +4,23 @@ export default class extends Controller {
   static targets = [ "popover", "htmlPopover" ]
 
   connect() {
+    this.instanciatePopovers()
+    this.instanciateHtmlPopovers()
+  }
+
+  instanciatePopovers() {
     this.popoverTargets.map((popover) => {
       new bootstrap.Popover(popover)
     })
+  }
 
+  instanciateHtmlPopovers() {
     this.htmlPopoverTargets.map((popover) => {
       new bootstrap.Popover(
         popover,
         {
           html: true,
-          sanitize: false
+          sanitize: false,
         }
       )
     });
